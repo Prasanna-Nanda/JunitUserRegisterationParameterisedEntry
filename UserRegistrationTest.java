43d5d7ac8546b84999a3543320def6595bc06f05
Prasanna-Nanda <prasannanandakumar92@gmail.com>
2021-02-28 15:02:11 +0530

    Added UserRegister

---
+++ UserRegistrationTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertSame;
+
+public class UserRegistrationTest {
+
+    @Test
+    public void  welcomeMessageTest() {
+        UserRegistration userRegistration=new UserRegistration();
+        String welcomeMessageResult=userRegistration.welcomeMessage("Welcome to user registration");
+        assertSame("Welcome to user registration",welcomeMessageResult);
+    }
+}
